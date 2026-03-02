f999e466e4bfb0bad935b744950c4e750a253b5e
SnWalker <lf109103@163.com>
2026-03-02 13:37:03 +0800

    docs:完善step1学习笔记，修复BeanFactory改动，并忽略idea配置

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 简易Bean对象的管理容器BeanFactory。
+ * <p>
+ * 目前仅实现了单例对象的注册与获取功能。
+ * 核心数据结构为 {@code Map<String, Object>}，用于缓存 Bean 实例。
  *
+ * @author SnWalker
  */
 public class BeanFactory {
     private Map<String, Object> beanMap = new HashMap<>();
